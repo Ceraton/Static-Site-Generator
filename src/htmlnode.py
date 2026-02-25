c41a686e9ef1e1fff32077596ea51c6948816ce2
ceraton <eric.miller1010@gmail.com>
2026-02-25 16:53:44 -0600

    ParentNode and Test cases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,7 +30,10 @@ class HTMLNode():
 
 class LeafNode(HTMLNode):
     def __init__(self, tag=None, value=None, props=None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, value, None, props)
+        self.tag = tag
+        self.value = value
+        self.props = props
     
     def to_html(self):
         if self.value is None:
@@ -47,3 +50,24 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+        self.tag = tag
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Missing tag")
+        if self.children is None:
+            raise ValueError("Missing children")
+        result = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children  :
+            #if child.value is None:
+                #raise ValueError("Missing Children")
+            result += child.to_html()
+        
+        result += f"</{self.tag}>"
+        return result
